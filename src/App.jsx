cf160e2b21226d49fb1bff69cfeaa091d3da63e1
Pho3nyxX <60277772+Pho3nyxX@users.noreply.github.com>
2026-03-11 19:36:51 -0500

    create useLocalStorage custom hook to persist state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import Filter from "./components/Filter";
+import useLocalStorage from "./hooks/useLocalStorage";
 import "./styles/App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  // const [todos, setTodos] = useState(() => {
+  //   try {
+  //     const saved = localStorage.getItem("todos");
+  //     return saved ? JSON.parse(saved) : [];
+  //   } catch {
+  //     return [];
+  //   }
+  // });
+
+  const [todos, setTodos] = useLocalStorage("todos", []);
   const [filter, setFilter] = useState("all");
 
   const filteredTodos = todos.filter((todo) => {
@@ -13,6 +23,10 @@ function App() {
     if (filter === "active") return !todo.completed;
     return true;
   });
+
+  // useEffect(() => {
+  //   localStorage.setItem("todos", JSON.stringify(todos));
+  // }, [todos]);
 
   return (
     <div className='app'>
